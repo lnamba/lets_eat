8f27904232c34d5a211bf4c9ef7071c419389e91
Dashon-Hawkins <dashon.hawkins@gmail.com>
2017-04-25 11:11:49 -0700

    users.ejs file initial commit

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,22 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      client: 'pg',
+      connection: 'postgres://localhost/lets_eat',
+      migrations: {
+          directory: __dirname + '/db/migrations',
+        },
+      seeds: {
+          directory: __dirname + '/db/seeds',
+        },
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+          directory: __dirname + '/db/migrations',
+        },
+      seeds: {
+          directory: __dirname + '/db/seeds',
+        },
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
