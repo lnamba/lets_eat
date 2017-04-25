be82ef42d4e8ba6b47cbec1b3510b29d71fcb117
Lauren Namba <lnnamba@gmail.com>
2017-04-25 14:18:35 -0700

    created login routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/login');
+  res.redirect('/login/index');
 });
 
 // router.get('/login', function(req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -3,11 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/login');
+  res.redirect('/login/index');
 });
 
 router.get('/login', function(req, res, next) {
   res.render('login/index');
 });
-
-router.post('/')
