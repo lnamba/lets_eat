5482b459538a9900d6e9a32a67fa70be6c8fe772
Lauren Namba <lnnamba@gmail.com>
2017-04-27 19:04:53 -0700

    fixed errors on dash page

--- routes/dash.js
+++ routes/dash.js
@@ -3,6 +3,15 @@ var router = express.Router();
 var knex = require('../db/knex');
 var superuser = 0;
 
+router.post('/undo', function(req, res, next) {
+  console.log(req.body);
+  knex.raw(`UPDATE suggestions SET accept_meal = FALSE`)
+  .then(function() {
+    res.clearCookie('accepted_meal');
+    res.redirect(`/dash/${req.body.id}`)
+  })
+})
+
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
   superuser = req.params.id;
@@ -29,13 +38,27 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
+router.post('/:id/accepted', function(req, res, next) {
+  knex.raw(`UPDATE suggestions SET accept_meal = TRUE WHERE id = ${req.params.id}`)
+  .then(function() {
+    res.cookie('accepted_meal', true);
+    res.redirect(`/dash/${req.params.id}`)
+  })
+})
+
 router.post('/:id/edit', function(req, res, next) {
-  knex.raw(`SELECT * FROM users WHERE id = ${req.params.id}`)
-  .then(function(user) {
-    res.render('dash/confirmed', {
-      message: "Your information has been recorded.",
-      user: user.rows[0]
-    })
+  knex.raw(`UPDATE users SET
+    name = '${req.body.name}',
+    email = '${req.body.email}',
+    about = '${req.body.about}'
+    WHERE id = ${req.params.id}`)
+  .then(function() {
+    knex.raw(`SELECT * FROM users WHERE id = ${req.params.id}`).then(function(user) {
+      res.render('dash/confirmed', {
+        message: "Your information has been recorded.",
+        user: user.rows[0]
+      });
+    });
   });
 });
 
